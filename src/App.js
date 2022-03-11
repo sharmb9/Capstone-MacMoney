df8099ebc3b1ca1205bb8c6c7aa4926591f2c62c
skiddie <ace7qi@gmail.com>
2022-03-11 00:33:41 -0500

    added Npc component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import './App.css';
 import PopUpButton from './components/PopUpButton';
 import PopUp from './components/PopUp';
+import Home from './components/Home';
+import Npc from './components/Npc';
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>MacMoney</h1>
-      </header>
-      <p>
-      Hello welcome to <PopUpButton target="#mac">McMaster University</PopUpButton>! I am Shivam, your Student Mentor for this year. I will be helping you adjust to the new uni life with all thing finance. Let’s start with a basic tutorial on how the game will work and progress.
-      </p>
-      <PopUp id="mac">
-        McMaster University (McMaster or Mac) is a public research university in Hamilton, Ontario, Canada. It operates six academic faculties: the DeGroote School of Business, Engineering, Health Sciences, Humanities, Social Science, and Science. It is a member of the U15, a group of research-intensive universities in Canada.
+      <Npc target="#hint"/>
+      <PopUp id="hint">
+        This is a hint.
       </PopUp>
     </div>
   );
